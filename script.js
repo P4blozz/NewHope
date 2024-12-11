6284077684c03cd6864a2d857607f0adca6564e0
Pablo <0001070522@senaimgaluno.com.br>
2024-12-11 18:48:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ function checkout() {
     let message = 'Olá, gostaria de comprar os seguintes itens:\n\n ';
     cart.forEach(productId => {
         const product = productData[productId];
-        message += `?? ${product.ref}\n\n?? ${product.size}\n\n`;
+        message += `📦 ${product.ref}\n\n?? ${product.size}\n`;
     });
 
     const whatsappLink = `https://wa.me/5531985079718?text=${encodeURIComponent(message)}`;
